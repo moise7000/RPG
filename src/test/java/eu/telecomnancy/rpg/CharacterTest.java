5648f3d2e4fb967c30bc9a0fbd48b37fe34556ad
ewan.decima@telecomnancy.eu <ewan.decima@yahoo.com>
2024-11-20 09:17:22 +0100

    [~] Several test for gameCharcater

--- src/test/java/eu/telecomnancy/rpg/CharacterTest.java
+++ src/test/java/eu/telecomnancy/rpg/CharacterTest.java
@@ -1,6 +1,7 @@
 package eu.telecomnancy.rpg;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,46 @@ public class CharacterTest {
         Warrior warrior = new Warrior("test");
         assertEquals(0, warrior.getHealth());
         
+    }
+
+
+    @Test
+    void testGetName() {
+        Warrior warrior = new Warrior("Hercules");
+        assertEquals("Hercules", warrior.getName());
+    }
+
+
+    @Test
+    void testSetHealth() {
+        Warrior warrior = new Warrior("Hercules");
+        warrior.setHealth(10);
+        assertEquals(10, warrior.getHealth());
+
+    }
+
+    @Test
+    void testSetExperiencePoint() {
+        Warrior warrior = new Warrior("Hercules");
+        warrior.setExperiencePoints(10);
+        assertEquals(10, warrior.getExperiencePoints());
+    }
+
+    @Test
+    void testGetLevel() {
+        Warrior warrior = new Warrior("Hercules");
+        assertEquals(1, warrior.getLevel());
+    }
+
+    @Test
+    void testSetLevel() {
+        Warrior warrior = new Warrior("Hercules");
+
+        assertEquals(1, warrior.getLevel());
+
+        warrior.setLevel(2);
+
+        assertEquals(2, warrior.getLevel());
     }
 
     
