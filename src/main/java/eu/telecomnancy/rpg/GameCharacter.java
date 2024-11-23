98d132d8e3d6dddb22d07232c8ad43c5ba67c5e1
ewan.decima@telecomnancy.eu <ewan.decima@yahoo.com>
2024-11-23 19:23:02 +0100

    [~] Adding observers in gamecharacter class

--- src/main/java/eu/telecomnancy/rpg/GameCharacter.java
+++ src/main/java/eu/telecomnancy/rpg/GameCharacter.java
@@ -1,16 +1,23 @@
 package eu.telecomnancy.rpg;
 
 
+import eu.telecomnancy.rpg.Observers.CharacterEvent;
+import eu.telecomnancy.rpg.Observers.CharacterObserver;
+import eu.telecomnancy.rpg.Observers.CharacterSubject;
 import eu.telecomnancy.rpg.Strategy.CombatStrategy;
 import eu.telecomnancy.rpg.Strategy.NeutralStrategy;
 
-public abstract class GameCharacter implements Duplicable<GameCharacter> {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class GameCharacter implements Duplicable<GameCharacter>, CharacterSubject {
 
     private final String name;
     private int health;
     private int experiencePoints;
     private int level;
     private CombatStrategy combatStrategy;
+    private final List<CharacterObserver> observers;
 
 
     public GameCharacter(String name) {
@@ -19,7 +26,26 @@ public abstract class GameCharacter implements Duplicable<GameCharacter> {
         this.level = 1;
         this.health = 100;
         this.combatStrategy = new NeutralStrategy();
+        this.observers = new ArrayList<>();
     }
+
+    @Override
+    public void addObserver(CharacterObserver observer) {observers.add(observer);}
+
+    @Override
+    public void removeObserver(CharacterObserver observer) {observers.remove(observer);}
+
+    @Override
+    public void notifyObservers(CharacterEvent event) {
+        for(CharacterObserver observer : observers){
+            observer.update(this, event);
+        }
+    }
+
+
+
+
+
 
     public void setCombatStrategy(CombatStrategy combatStrategy) {this.combatStrategy = combatStrategy;}
 
@@ -33,6 +59,7 @@ public abstract class GameCharacter implements Duplicable<GameCharacter> {
 
         if (this.health <= 0) {
             this.health = 0;
+            notifyObservers(CharacterEvent.DEATH);
         }
     }
 
