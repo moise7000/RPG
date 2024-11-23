9f3dede9ac56565814f502adf5e650c1703d6065
ewan.decima@telecomnancy.eu <ewan.decima@yahoo.com>
2024-11-23 18:13:29 +0100

    [~] New implementation of Buff visitor

--- src/main/java/eu/telecomnancy/rpg/Visitors/DamageVisitor.java
+++ src/main/java/eu/telecomnancy/rpg/Visitors/DamageVisitor.java
@@ -1,17 +1,20 @@
 package eu.telecomnancy.rpg.Visitors;
 
 import eu.telecomnancy.rpg.CharacterVisitor;
+import eu.telecomnancy.rpg.GameConfiguration;
 import eu.telecomnancy.rpg.Warrior;
 import eu.telecomnancy.rpg.Wizard;
 
 public class DamageVisitor implements CharacterVisitor {
+    private final GameConfiguration config = GameConfiguration.getShared();
+
     @Override
     public void visit(Warrior warrior){
         //Values before visiting
         int currentHealth = warrior.getHealth();
 
         //Action
-        int damageAmount = 5;
+        int damageAmount = config.getBaseDamageAmount();
 
         //Set
         warrior.setHealth(currentHealth - damageAmount);
@@ -23,7 +26,7 @@ public class DamageVisitor implements CharacterVisitor {
         int currentHealth = wizard.getHealth();
 
         //Action
-        int damageAmount = 5;
+        int damageAmount = config.getBaseDamageAmount();
 
         //Set
         wizard.setHealth(currentHealth - damageAmount);
