bc643093c53bd51a78355a65de7d48e6450bc9b5
ewan.decima@telecomnancy.eu <ewan.decima@yahoo.com>
2024-11-23 13:24:25 +0100

    [+] New concrete strategy: AgressiveStrategy

--- src/main/java/eu/telecomnancy/rpg/Strategy/AgressiveStrategy.java
+++ src/main/java/eu/telecomnancy/rpg/Strategy/AgressiveStrategy.java
@@ -1,2 +1,18 @@
-package eu.telecomnancy.rpg.Strategy;public class AgressiveStrategy {
+package eu.telecomnancy.rpg.Strategy;
+
+public class AgressiveStrategy implements CombatStrategy {
+    private static final double DAMAGE_MULTIPLIER = 1.5;
+    private static final double DEFENSE_MULTIPLIER = 1.3;
+
+    @Override
+    public int calculateDamageDealt(int baseDamage){
+        return (int) (baseDamage * DAMAGE_MULTIPLIER);
+    }
+
+    @Override
+    public int calculateDamageReceived(int inCommingDamage) {
+        return (int) (inCommingDamage * DEFENSE_MULTIPLIER);
+    }
+
+
 }
