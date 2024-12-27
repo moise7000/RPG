81cb755c3be66ca18cdb766308aaa51853c220a0
ewan.decima@telecomnancy.eu <ewan.decima@yahoo.com>
2024-12-27 16:46:06 +0100

    [+] Quit button in main menu

--- src/main/java/gameInterface/Scenes/MainMenuScene.java
+++ src/main/java/gameInterface/Scenes/MainMenuScene.java
@@ -2,6 +2,7 @@ package gameInterface.Scenes;
 
 import gameInterface.InterfaceConfiguration;
 import gameInterface.Main;
+import javafx.application.Platform;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
@@ -12,13 +13,19 @@ public class MainMenuScene {
         Button playButton = new Button(config.getPlayButtonLabel());
         Button settingsButton = new Button(config.getSettingsButtonLabel());
         Button creditsButton = new Button(config.getCreditsButtonLabel());
+        Button closeButton = new Button(config.getQuitButtonLabel());
 
         playButton.setStyle(config.getButtonStyle());
         settingsButton.setStyle(config.getButtonStyle());
         creditsButton.setStyle(config.getButtonStyle());
+        closeButton.setStyle(config.getButtonStyle());
 
 
-        VBox menu = new VBox(20, playButton, settingsButton, creditsButton);
+
+
+
+
+        VBox menu = new VBox(20, playButton, settingsButton, creditsButton, closeButton);
         menu.setTranslateY(100);
         menu.setStyle("-fx-alignment: center;");
 
@@ -26,7 +33,7 @@ public class MainMenuScene {
         creditsButton.setOnAction(e -> mainApp.setSceneContent(CreditsScene.create(mainApp, config)));
         settingsButton.setOnAction(e -> mainApp.setSceneContent(SettingsScene.create(mainApp, config)));
         playButton.setOnAction(e -> mainApp.setSceneContent(CharacterSelectionScene.create(mainApp, config)));
-
+        closeButton.setOnAction(e -> Platform.exit());
         return menu;
     }
 }
