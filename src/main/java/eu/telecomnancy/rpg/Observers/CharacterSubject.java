b560da31146650947bbe55c35bf101b781641163
ewan.decima@telecomnancy.eu <ewan.decima@yahoo.com>
2025-01-12 19:15:47 +0100

    [+] fatjar

--- src/main/java/eu/telecomnancy/rpg/Observers/CharacterSubject.java
+++ src/main/java/eu/telecomnancy/rpg/Observers/CharacterSubject.java
@@ -2,6 +2,7 @@ package eu.telecomnancy.rpg.Observers;
 
 
 
+
 public interface CharacterSubject {
     void addObserver(CharacterObserver observer);
     void removeObserver(CharacterObserver observer);
